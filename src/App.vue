bab9dfd401008f9b76b38106a576a30f7b9b3175
James Fraser <jimbob4t@virginmedia.com>
2020-01-17 17:25:35 +0000

    info displaying on screen

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template lang="html">
   <div class="">
     <h1>World of Football</h1>
-    <!-- <footballers-list-item/> -->
-    <!-- </FootballersListItem> -->
+    <div class="">
+      <footballers-list :footballers="footballers"></footballers-list>
+    </div>
+
+
   </div>
 
 </template>
 
 <script>
-
+import FootballersList from './components/footballers_list.vue'
 
 
 export default {
   name: 'app',
   data() {
     return {
-      footballers:[]
+      footballers:[],
+      footballers_list: null
+
     }
   },
 
@@ -23,10 +28,12 @@ mounted() {
   fetch('https://www.scorebat.com/video-api/v1/')
   .then(response => response.json())
   .then(footballers => this.footballers = footballers);
+
 },
 components: {
-  // 'footballers-list-item' : FootballersListItem
-},
+  'footballers-list' : FootballersList
+
+}
 
 }
 
---
+++ src/components/footballers_list.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="">
+<ul>
+  <li v-for="(footballer, index) in footballers" :value="footballer" :key="index">{{ footballer.side1.name }}</li>
+</ul>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'footballers-list',
+  props: ['footballers'],
+  methods:{
+
+  }
+
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
